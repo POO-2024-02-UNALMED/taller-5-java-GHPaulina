5b8c0644733540b76ef4247e26432b4b74089288
GHPaulina <pgomezh@unal.edu.co>
2024-12-09 21:33:55 -0500

    Taller

--- src/main/java/zooAnimales/Reptil.java
+++ src/main/java/zooAnimales/Reptil.java
@@ -2,29 +2,29 @@ package zooAnimales;
 import java.util.ArrayList;
 
 
-public class Pez extends Animal{
-	private static ArrayList<Pez> listado=new ArrayList<>();
-	public static int salmones;
-	public static int bacalaos;
+public class Reptil extends Animal{
+	private static ArrayList<Reptil> listado=new ArrayList<>();
+	public static int iguanas;
+	public static int serpientes;
 	private String colorEscamas;
-	private int cantidadAletas;
+	private int largoCola;
 	
-	public Pez() {
+	public Reptil() {
 		this(null,0,null,null,null,0);
 	}
 	
-	public Pez(String nombre, int edad, String habitat, String genero, String colorEscamas, int cantidadAletas) {
+	public Reptil(String nombre, int edad, String habitat, String genero, String colorEscamas, int largoCola) {
 		super(nombre,edad,habitat,genero);
 		this.colorEscamas=colorEscamas;
-		this.cantidadAletas=cantidadAletas;
-		Pez.listado.add(this);
+		this.largoCola=largoCola;
+		Reptil.listado.add(this);
+		
 	}
 	
-	
-	public static void setListado(ArrayList<Pez> listado) {
-		Pez.listado=listado;
+	public static void setListado(ArrayList<Reptil> listado) {
+		Reptil.listado=listado;
 	}
-	public static ArrayList<Pez> getListado() {
+	public static ArrayList<Reptil> getListado() {
 		return listado;
 	}
 	
@@ -35,37 +35,34 @@ public class Pez extends Animal{
 		return colorEscamas;
 	}
 	
-	public void setCantidadAletas(int cantidadAletas) {
-		this.cantidadAletas=cantidadAletas;
-		
+	public void setLargoCola(int largoCola) {
+		this.largoCola=largoCola;
 	}
-	public int getCantidadAletas() {
-		return cantidadAletas;
+	public int getLargoCola() {
+		return largoCola;
 	}
 	
-	
-	public int cantidadPeces() {
-		return Pez.listado.size();
+	public int cantidadReptiles() {
+		return Reptil.listado.size();
 		
 	}
 	
 	public String movimiento() {
-		return "nadar";
+		return "reptar";
 	}
 	
-	public static Pez crearSalmon(String nombre, int edad, String genero) {
-		Pez Pez=new Pez(nombre, edad,"oceano", genero, "rojo", 6);
-		salmones++;
-		return Pez;
+	public static Reptil crearIguana(String nombre, int edad, String genero) {
+		Reptil Reptil= new Reptil(nombre, edad, "humedal",genero, "verde",3);
+		iguanas++;
+		return Reptil;
 		
 	}
 	
-	public static Pez crearBacalao(String nombre, int edad, String genero) {
-		Pez Pez=new Pez(nombre, edad, "oceano", genero, "gris", 6);
-		bacalaos++;
-		return Pez;
+	public static Reptil crearSerpiente(String nombre, int edad, String genero) {
+		Reptil Reptil =new Reptil(nombre, edad, "jungla", genero, "blanco", 1);
+		serpientes++;
+		return Reptil;
+		
 	}
-	
-
 
 }
